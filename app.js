c133ca9effe7075860ec55ab3f00944a78f4c6f6
ayaz1o1 <ayaz_ansari1983@yahoo.com>
2020-07-11 23:47:24 +0500

    Update app.js
    Function added

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 
+ var c = 0;
+var b=   0;
+function myCounter() {
+document.getElementById("demo2").innerHTML = b; 
+document.getElementById("demo").innerHTML = ++c ;
+  if ( c == 10 ){
+  c= 0;
+  b = ++b;
+  }
+}
